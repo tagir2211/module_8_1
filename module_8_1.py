3036f4a9bb20ab39bc007016576258aaa1f7ad0c
tagir2211 <tagir2211@gmail.com>
2024-09-21 10:24:12 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,20 @@
+try:
+    def add_everything_up(a, b):
+        a = float(a)
+        b = float(b)
+        if a % 10 == 0:
+            a = int(a)
+        if b % 10 == 0:
+            b = int(b)
+        return a + b
+    first = input('Введите первый элемент: ')
+    second = input('Введите второй элемент: ')
+
+    print(add_everything_up(first, second))
+except (TypeError, ValueError):
+    print(first + second)
+
+finally:
+    print('end')
+
+
